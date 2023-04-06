76b2e141eb5aa30732c8247e5680d3e2877b4067
Osireg17 <osireg@hotmail.com>
2023-04-06 17:48:09 +0100

    Add auth in firebase, and authchange

--- client/team-6/src/Pages/Business_Pages/BusinessAuth.tsx
+++ client/team-6/src/Pages/Business_Pages/BusinessAuth.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import logo from '/Personal Programming Projects/CodeForGood23/Team-6/client/team-6/src/Photos/My project.png'
-
+import {auth, db} from '/Personal Programming Projects/CodeForGood23/Team-6/client/team-6/src/firebase/firebase'
+import {createUserWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth'
+import {collection, addDoc} from 'firebase/firestore'
+import {redirect} from "react-router-dom"
 const BusinessAuth = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [name, setName] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (password === confirmPassword) {
+            await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+        }
+        else {
+            setError('Passwords do not match')
+        }
+
+        const docRef = await addDoc(collection(db, "users"), {
+            name: name,
+            email: email,
+            password: password,
+            type: 'business'
+        })
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // if user is logged in, redirect to the business dashboard
+            redirect('/business/dashboard')
+            // display hello user name in console
+            console.log('hello', user)
+        }
+        else{
+            // if user is not logged in, redirect to the business login page
+            redirect('/Auth/business')
+        }
+            
+    })
+    
   return (
     <div>
        <> 
@@ -15,43 +65,48 @@ const BusinessAuth = () => {
                       Create a business account
                     </h2>
                     </div>
-                    <form className="flex flex-col pt-3 md:pt-8">
+                    <form onSubmit={handleSubmit} className="flex flex-col pt-3 md:pt-8">
                         <div className="flex flex-col pt-4">
                         <label  className="block mb-2 text-sm font-medium text-black">Name</label>
                             <div className="flex relative ">
-                                <input type="text" id="design-login-email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Email"/>
+                                <input onChange={(e) => setName(e.target.value)} type="text" id="design-login-email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Email"/>
                             </div>
                         </div>
                         <div className="flex flex-col pt-4">
                         <label  className="block mb-2 text-sm font-medium text-black">Email</label>
                             <div className="flex relative ">
-                                <input type="text" id="design-login-email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Email"/>
+                                <input onChange={(e) => setEmail(e.target.value)} type="text" id="design-login-email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Email"/>
                             </div>
                         </div>
                         <div className="flex flex-col pt-4">
                         <label  className="block mb-2 text-sm font-medium text-black">Password</label>
                             <div className="flex relative ">
-                                <input type="password" id="design-login-password" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Password"/>
+                                <input onChange={(e) => setPassword(e.target.value)} type="password" id="design-login-password" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Password"/>
                             </div>
                         {/* tell users to add special characters to the their password */}
-                        <label className="block mb-2 text-xs font-medium text-black">Password must contain at least 8 characters</label>
+                        <label className="block text-xs font-medium text-black">Password must contain at least 8 characters</label>
                         </div>
                         <div className="flex flex-col pt-4">
                         <label  className="block mb-2 text-sm font-medium text-black">Confirm Password</label>
                             <div className="flex relative ">
-                                <input type="password" id="design-login-password" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Password"/>
+                                <input onChange={(e) => setConfirmPassword(e.target.value)} type="password" id="design-login-password" className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-[#dce3eb] text-gray-700 rounded-md placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent" placeholder="Password"/>
                             </div>
                         </div>
-
-                        <div className="pt-12 pb-12 text-center">
+                        <div className="pt-4 pb-8 text-center">
                         <p>
                             Already have a business account? 
                             <Link to="/Auth/Login" className="font-semibold text-black hover:text-red-600">
                                 Login
                             </Link>
                         </p>
+                        <p className='pt-2'>
+                            Are you a customer?  
+                            <Link to="/Auth/User" className="font-semibold text-black hover:text-red-600">
+                                Register Now
+                            </Link>
+                        </p>
                         </div>
-                        <button type="submit" className="w-full mb-2 px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black rounded-md shadow-md hover:text-black hover:bg-black focus:outline-none focus:ring-2">
+                        <button type="submit" className="w-full  px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-black rounded-md shadow-md hover:text-black hover:bg-black focus:outline-none focus:ring-2">
                             <span className="w-full text-white">
                                 Submit
                             </span>
--- client/team-6/src/firebase/firebase.js
+++ client/team-6/src/firebase/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "@firebase/firestore";
-
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -13,11 +13,15 @@ const firebaseConfig = {
   projectId: "cfg-team-6",
   storageBucket: "cfg-team-6.appspot.com",
   messagingSenderId: "116818881955",
-  appId: "1:116818881955:web:c898fd12f56b513b94fe16",
-  measurementId: "G-7J8XTNHNNR"
+  appId: "1:116818881955:web:d754b3803799c95894fe16",
+  measurementId: "G-749698PW6E"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+
+const db = getFirestore();
+const auth = getAuth(app);
+
+export { db, auth };
+
