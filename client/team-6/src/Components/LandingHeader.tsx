99898c8fe55d8ad0ed70f24d63c4299f39664cb3
r4chelpham <r4chelpham@gmail.com>
2023-04-06 17:45:44 +0100

    Styling and created components of landing page

--- client/team-6/src/Components/LandingHeader.tsx
+++ client/team-6/src/Components/LandingHeader.tsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import ResLogo from '../photos/res-logo.png'
 
 const LandingHeader = () => {
@@ -10,7 +10,7 @@ const LandingHeader = () => {
 
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-gray-900 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center">
           <img
@@ -23,7 +23,7 @@ const LandingHeader = () => {
           data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          onClick = {toggleMenu}
+          onClick={toggleMenu}
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -42,31 +42,30 @@ const LandingHeader = () => {
             />
           </svg>
         </button>
-    <div className={`${
-            open ? 'block' : 'hidden'
+        <div className={`${open ? 'block' : 'hidden'
           } w-full md:block md:w-auto mt-4 md:mt-0`}
-          >
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-        <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Sign up
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Log in
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-900 rounded-lg bg-gray-900 md:flex-row md:space-x-8 md:mt-0 md:border-0 md: dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-100 rounded bg-gray-900 hover:bg-green-700 md:hover:bg-gray-900 md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Sign up
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-100 rounded bg-gray-900 hover:bg-green-700 md:hover:bg-gray-900 md:border-0 md:hover:text-green-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Log in
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
 
   )
