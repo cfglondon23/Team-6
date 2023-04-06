9e413d85dcd76e3920462c0d55ba3288ee37bc9b
Osireg17 <osireg@hotmail.com>
2023-04-06 12:42:25 +0100

    I added a lot

--- client/team-6/src/App.tsx
+++ client/team-6/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Auth from './Pages/Auth';
+import BusinessAuth from './Pages/Business_Pages/BusinessAuth';
+import UserAuth from './Pages/User_Pages/UserAuth';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-black text-4xl text-center'>
-        Team 6
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/Auth" element={<Auth/>} />
+        <Route path="/Auth/business" element={<BusinessAuth/>} />
+        <Route path='/Auth/User' element={<UserAuth/>} />
+      </Routes>
+    </Router>
   );
 }
 
