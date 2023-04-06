4d518a0d967df24763e32d01c65ed78972b87b68
Osireg17 <osireg@hotmail.com>
2023-04-06 11:42:14 +0100

    Added tailwindCSS for styling Added Firebase Added Pages and Components folders

---
+++ client/team-6/src/firebase/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "@firebase/firestore";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJoUxc-V1SZbhwg6Kcx1dDYLHwzI432LM",
+  authDomain: "cfg-team-6.firebaseapp.com",
+  projectId: "cfg-team-6",
+  storageBucket: "cfg-team-6.appspot.com",
+  messagingSenderId: "116818881955",
+  appId: "1:116818881955:web:c898fd12f56b513b94fe16",
+  measurementId: "G-7J8XTNHNNR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
